64d67b90e8adc485fd47e4a54782a892ed1a64eb
nisenogueira <nisenogueira2013@gmail.com>
2024-10-04 00:39:32 +0000

    Apache v.1.0

---
+++ prov-serv-web.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+echo "Atualizando o servidor..."
+
+apt-get update
+apt-get upgrade -y
+
+acho "Instalando  aplicações..."
+apt-get install apache2 unzip -y
+apt-get install unzip -y
+
+echo "Baixando e  copiando arquivos..."
+cd /tmp
+wget https://github.com/denilsonbonatti/linux-site-dio/archive/refs/heads/main.zip
+unzip main.zip
+cd linux-site-dio-main
+cp -R * /var/www/html/
+
+
+
